aaecbad5dc027b736ae4fe2aa52e5222498bd995
Matthias Wurm <wurm.matthias@gmx.net>
2020-03-22 12:31:18 +0100

    add:Imprint & Privacy pages

---
+++ src/app/pages/imprint-page/imprint-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imprint-page',
+  templateUrl: './imprint-page.component.html'
+})
+export class ImprintPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
